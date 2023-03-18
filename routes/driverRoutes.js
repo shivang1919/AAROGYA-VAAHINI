ab240cbaeb4e0e831533b8cf83df3fe0eb30887f
Shivang Sharma <95305111+shivang1919@users.noreply.github.com>
2023-03-19 03:48:50 +0530

    Delete routes directory

--- routes/driverRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { registerDriver, loginDriver } = require('../controllers/driverController');
-const router = express.Router();
-
-router.post('/register',registerDriver)
-router.post('/login',loginDriver)
-module.exports = router
