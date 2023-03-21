4ad0fa9d13667b8636544f829f46d15f298b3bf9
Shubham Kumar Sinha <shubhamsinhaasus@gmail.com>
2023-03-21 22:20:34 +0530

    changed frontpage ui

--- ambulance/src/components/frontpage/Frontpage.js
+++ ambulance/src/components/frontpage/Frontpage.js
@@ -1,35 +1,38 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 export default function Frontpage() {
 
     return (
+
         <div className="bg-right bg-no-repeat font-sans relative flex flex-col justify-center min-h-screen overflow-hidden h-full w-full bg-gray-400  backdrop-filter backdrop-blur-sm bg-opacity-5 ">
-            <div className="w-full bg-[#1C2530] p-6 m-auto rounded-md border-2 border-gray-100 shadow-[0px_0px_40px_rgba(0,0,0,0.8)] shadow-zinc-400 lg:max-w-xl">
-                <h1 className="text-3xl tracking-wider font-semibold text-center text-white">
-                   AAROGYA VAHINI
-                </h1>
-                <form className="my-6 flex justify-center flex-col place-items-center">
-                    
-                    <div className="mt-6">
+            <h1 className="text-right text-7xl tracking-wider font-semibold font-serif text-red-600 mr-20 mb-70 ">
+                Aarogya<span className="text-7xl tracking-wider font-semibold font-serif text-white ml-4 mb-8">Vahini</span>
+            </h1>
+            <div className="mb-32">
+                <p className="pFont text-start fixed ml-60 text-7xl tracking-wider font-semibold text-center text-white mt-16">
+                    24/7
+                </p>
+                <p className="pFont text-start fixed ml-24 text-5xl tracking-wider font-extrabold text-center text-white mt-40 mb-20">
+                    <span className="text-red-600">Emergency </span>Service
+                </p>
+                <p className="pFont text-start fixed text-2xl tracking-wider font-semibold text-center text-white mt-72 ml-20">
+                    Now get your ambulance at<br></br> your footstep.
+                </p>
+                    <div className="items-left content-start mt-32 mr-auto">
                         <NavLink to="users/login/">
-                        <button className="text-lg px-36 py-2 tracking-wider text-white transition-colors duration-100 transform bg-blue-700 rounded-md hover:bg-blue-800 focus:outline-none">
-                            LOGIN AS A USER
-                        </button>
-                        </NavLink>
-                    </div>
 
-                    <div className="mt-6">
+                            <button className="text-lg w-48 py-2 tracking-wider text-white transition-colors duration-100 transform bg-blue-800 rounded-md hover:bg-white focus:outline-none hover:text-black hover:font-semibold ml-32 mr-4 mt-64">
+                            
+                                USER LOGIN
+                            </button>
+                        </NavLink>
                         <NavLink to="drivers/login/">
-                        <button className="text-lg px-36 pr-32 py-2 tracking-wider text-white transition-colors duration-100 transform bg-blue-700 rounded-md hover:bg-blue-800 focus:outline-none">
-                            LOGIN AS A DRIVER 
-                        </button>
+                            <button className="items-left text-lg w-48 py-2 tracking-wider text-white transition-colors duration-100 transform bg-blue-800 rounded-md hover:bg-white focus:outline-none hover:text-black hover:font-semibold">
+                                DRIVER LOGIN
+                            </button>
                         </NavLink>
                     </div>
 
-                    
-                </form>
-
-                
             </div>
         </div>
     );
