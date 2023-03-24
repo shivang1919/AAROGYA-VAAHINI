66064d8f53f67162131a12a35500bdde2822f635
shivang <shivangnov@gmail.com>
2023-03-24 16:25:57 +0530

    added user map

--- ambulance/src/components/mappages/Usermap.js
+++ ambulance/src/components/mappages/Usermap.js
@@ -1,106 +1,112 @@
 /*global google*/
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
-
+let iconMarker = new window.google.maps.MarkerImage(
+    "https://img.lovepik.com/element/45000/6349.png_300.png",
+    null, /* size is determined at runtime */
+    null, /* origin is 0,0 */
+    null, /* anchor is bottom center of the scaled image */
+    new window.google.maps.Size(32, 32)
+);
 const mapStyles = {
     width: '80%',
     height: '80%'
 };
 
-const showdrivers = async (e) => {
-    e.preventDefault();
-    const res = await fetch("https://aarogya-vaahini-api.vercel.app/api/drivers/getavailabledrivers", {
-        method: "GET",
-        headers: {
-            "content-Type": "application/json"
-        },
-    })
-    const dridata = await res.json();
-    return dridata
-
-    // localStorage.setItem("availibledriver",JSON.stringify(dridata))
-}
-var driverda = showdrivers()
-console.log(driverda)
 
 var userda = JSON.parse(localStorage.getItem("userdata"));
 console.log(userda.updatedUser.latitude)
-function Mappo() {
-    const [activeMarker,setActiveMarker] = useState(null)
-    const handleActiveMarker = (marker)=>{
-        if(marker === activeMarker){
-            return;
+
+const Usermap = () => {
+    const [driver, setDriver] = useState([])
+    const showdrivers = async (e) => {
+        e.preventDefault();
+        const res = await fetch("https://aarogya-vaahini-api.vercel.app/api/drivers/getavailabledrivers", {
+            method: "GET",
+            headers: {
+                "content-Type": "application/json"
+            },
+        })
+        const dridata = await res.json();
+        setDriver(dridata?.availableDriver)
+        return dridata
+
+        // localStorage.setItem("availibledriver",JSON.stringify(dridata))
+    }
+    function Mappo() {
+        const [activeMarker, setActiveMarker] = useState(null)
+        const handleActiveMarker = (marker) => {
+            if (marker === activeMarker) {
+                return;
+            }
+            setActiveMarker(marker)
         }
-        setActiveMarker(marker)
-    }
-    const handleOnLoad = (map) => {
-        const bounds = new google.maps
-    }
-    return (
-        <Map google={window.google}
-            zoom={14}
-            style={mapStyles}
-            initialCenter={
-                {
+        const handleOnLoad = (map) => {
+            const bounds = new google.maps
+        }
+        return (
+            <Map google={window.google}
+                zoom={14}
+                style={mapStyles}
+                initialCenter={
+                    {
+                        lat: userda.updatedUser.latitude,
+                        lng: userda.updatedUser.longitude
+                    }
+                }
+            >
+                <Marker position={{
                     lat: userda.updatedUser.latitude,
                     lng: userda.updatedUser.longitude
+                }}
+                    name={userda.updatedUser.name}
+                >
+                </Marker>
+                {
+                    driver?.map((e) => {
+                        return (
+                            <Marker position={{
+                                lat: e?.latitude,
+                                lng: e?.longitude
+
+                            }}
+                            icon={iconMarker}
+                            />
+                        )
+                    })
+
+
                 }
-            }
-        >
-            <Marker position={{
-                lat: userda.updatedUser.latitude,
-                lng: userda.updatedUser.longitude
-            }}
-                name={userda.updatedUser.name}
-            >
-            </Marker>
-            {/* {
-                driverda.availibleDriver.map((e)=>{
-                    <Marker 
-                    position={{lat:driverda[e].latitude,lng:driverda[e].longitude}}
-                    />
-
-                    
-                })
-            } */}
-        
-            
-
-        </Map>
-    )
-}
-const Usermap = () => {
 
 
 
+
+            </Map>
+        )
+    }
+
+
+
+    console.log(driver)
     return (
         <>
 
             <div className='flex-row'>
-                <div className='position-relative'>
-                    <Mappo />
-                </div>
                 <div>
                     <button className="bg-blue-500 items-center z-10 position:relative  hover:bg-blue-700 justify-center text-white font-bold py-2 px-4 rounded mt-75px" onClick={showdrivers}>
                         Button
                     </button>
                 </div>
+                <div className='position-relative'>
+                    <Mappo />
+                </div>
 
             </div>
         </>
     )
-    // return (
-    //     <>
-    //         <Map />
-    //         <div >
-    //             <button className="bg-blue-500 items-center hover:bg-blue-700 justify-center text-white font-bold py-2 px-4 rounded" onClick={showdrivers}>
-    //                 Button
-    //             </button>
-    //         </div>
-    //     </>
-    // )
+
 }
 
 export default GoogleApiWrapper({
-    // apiKey: process.env.GOOGLE_MAPS_API
+    apiKey: process.env.GOOGLE_MAPS_API
 })(Usermap);
